573bc05d65a11c58084608587929f1188fa69ebb
Ayesh Karunaratne <ayesh@ayesh.me>
2020-04-23 03:30:56 +0700

    Fix dma alias for dump-min-autoload command

--- src/DumpAutololoadMinCommand.php
+++ src/DumpAutololoadMinCommand.php
@@ -12,8 +12,7 @@ class DumpAutololoadMinCommand extends DumpAutoloadCommand {
     {
         parent::configure();
         $this->setName('dump-min-autoload');
-        $this->setAliases(['dmo']);
-        $this->setAliases(['dump-mini-autoload']);
+        $this->setAliases(['dma', 'dump-mini-autoload']);
         $this->setDescription('Dumps a minimal version of the autoloader');
         $this
             ->setDefinition(array(
